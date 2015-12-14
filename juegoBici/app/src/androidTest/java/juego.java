5bc693d6dc8967819f969b80a749fd1013720e25
macortes <miguelcortgil@gmail.com>
2015-12-14 13:48:14 +0100

    Juego Version1 Bici

---
+++ juegoBici/app/src/androidTest/java/juego.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mati on 14/12/15.
+ */import android.app.Activity;
+import android.os.Bundle;
+
+public class Juego extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego);
+    }
+}
+public class juego {
+}
